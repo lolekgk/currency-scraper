c310329a31a11e5e9692e0893812740d639a4794
Karol Gajda <karol.gajda97@gmail.com>
2022-10-20 20:15:30 +0200

    Add endpoint for single currency

--- currency_scraper/main.py
+++ currency_scraper/main.py
@@ -5,7 +5,13 @@ from scraper import scrap_currencies_from_html
 app = FastAPI()
 
 
-@app.get("/currency/{currency_code}")
-async def fetch_currency(currency_code):
+@app.get("/currencies")
+def fetch_all_currencies():
     currencies = scrap_currencies_from_html()
-    return currencies[currency_code.upper()]
+    return currencies
+
+
+@app.get("/currencies/{currency_code}")
+def fetch_currency(currency_code):
+    currencies = scrap_currencies_from_html()
+    return currencies.get(currency_code.upper())
