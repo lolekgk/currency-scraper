c7339bea64d9bbc6974c75962dacbd6ed10877b4
Karol Gajda <karol.gajda97@gmail.com>
2022-11-05 11:58:07 +0100

    Refactor UserFriendlyCurrency

--- currency_scraper/models.py
+++ currency_scraper/models.py
@@ -9,3 +9,7 @@ class Currency(BaseModel):
     convertion_rate: int
     currency_avg_rate: float
     date: datetime.datetime
+
+
+class UserFriendlyCurrency(Currency):
+    date: datetime.date  # date instead of datetime with a lot of 0
