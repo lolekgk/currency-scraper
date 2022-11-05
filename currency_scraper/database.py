7debb63208d1f1e53e125685fc91ab4f0cf333b2
Karol Gajda <karol.gajda97@gmail.com>
2022-11-05 13:34:34 +0100

    Refactor date filter query

--- currency_scraper/database.py
+++ currency_scraper/database.py
@@ -1,9 +1,8 @@
 import os
-from datetime import date, datetime, timedelta
+from datetime import date, datetime
 
 from pymongo import MongoClient
 
-from currency_scraper.scraper import NbpCurrencyScrapper
 from currency_scraper.singleton import Singleton
 
 
@@ -15,42 +14,42 @@ class Database(metaclass=Singleton):
         self.db = self.client['currencies_db']
         self.currency_collections = self.client['currencies_db']['currencies']
 
+    @staticmethod
+    def _get_date_filter(date: date):
+        return {
+            'date': {
+                '$gte': date,
+                '$lte': date,
+            }
+        }
+
     def get_all_currencies(self):
-        self.insert_currencies()
         result = self.currency_collections.find({}, {'_id': 0}).sort(
             'date', -1
         )
         return list(result)
 
     def get_currency_from_all_dates(self, currency_code: str) -> list:
-        self.insert_currencies()
         query = {'currency_code': currency_code.upper()}
         result = self.currency_collections.find(query, {'_id': 0}).sort(
             'date', -1
         )
         return list(result)
 
-    def insert_currencies(self):
-        scraper = NbpCurrencyScrapper()
-        date = scraper.scrap_publication_date()
-        query = {
-            'date': {
-                '$gte': date,
-                '$lt': date + timedelta(seconds=1),
-            }
-        }
-        if not self.currency_collections.find_one(query):
-            currencies = scraper.scrap_currencies_with_publication_date()
-            self.currency_collections.insert_many(currencies)
-
     def get_currency_from_provided_date(self, currency_code: str, date: date):
-        date = datetime.combine(date, datetime.min.time())
-        query = {
-            'date': {
-                '$gte': date,
-                '$lt': date + timedelta(seconds=1),
-            },
-            'currency_code': currency_code.upper(),
+        date = datetime.combine(
+            date, datetime.min.time()
+        )  # convert date(pymongo does not support it) to datetime
+        query = Database._get_date_filter(date) | {
+            'currency_code': currency_code.upper()
         }
         result = self.currency_collections.find_one(query, {'_id': 0})
         return result
+
+    def is_date_in_database(self, date: datetime):
+        query = Database._get_date_filter(date)
+        return self.currency_collections.find_one(query)
+
+    def insert_currencies(self, currencies: list[dict]):
+        self.currency_collections.insert_many(currencies)
+        return currencies
