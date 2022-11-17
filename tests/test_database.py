2fc12c374a0f9a9f0a02a3371856f563e9d94de7
Karol Gajda <karol.gajda97@gmail.com>
2022-11-17 20:45:24 +0100

    Add helper and insert functions tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date, datetime
 from unittest.mock import MagicMock, Mock, patch
 
 import pytest
@@ -47,6 +47,13 @@ def expected_usd_result_fix():
 
 
 @pytest.fixture
+def date_fix():
+    test_date = date(2022, 10, 10)
+    yield test_date
+    del test_date
+
+
+@pytest.fixture
 def mocked_db_data_fix(usd_currency, eur_currency):
     mocked_db_data = [usd_currency, eur_currency]
     yield mocked_db_data
@@ -68,6 +75,30 @@ class TestDatabase:
     def test_database_init(self, mocked_database_fix, mocked_db_data_fix):
         database_object = Database()
         assert database_object.currency_collections == mocked_db_data_fix
+
+    def test_get_date_filter(self, mocked_database_fix, date_fix):
+        database_object = Database()
+        result = database_object._get_date_filter(date_fix)
+        assert result == {
+            'date': {
+                '$gte': date_fix,
+                '$lte': date_fix,
+            }
+        }
+
+    def test_get_currency_filter_with_lowercase_code(
+        self, mocked_database_fix
+    ):
+        database_object = Database()
+        result = database_object._get_currency_filter(currency_code='abc')
+        assert result == {'currency_code': 'ABC'}
+
+    def test_get_currency_filter_with_upperrcase_code(
+        self, mocked_database_fix
+    ):
+        database_object = Database()
+        result = database_object._get_currency_filter(currency_code='ABC')
+        assert result == {'currency_code': 'ABC'}
 
     def test_get_all_currencies(
         self, mocked_database_fix, usd_currency, expected_usd_result_fix
@@ -92,3 +123,50 @@ class TestDatabase:
             currency_code='USD'
         )
         assert result == expected_usd_result_fix
+
+    def test_get_currnect_from_provided_date(
+        self, mocked_database_fix, date_fix, eur_currency
+    ):
+        database_object = Database()
+        database_object._get_date_filter.return_value = 'mocked'
+        database_object._get_currency_filter.return_value = 'mocked'
+        mocked_collection = Mock()
+        mocked_collection.find_one.return_value = eur_currency
+        database_object.currency_collections = mocked_collection
+        result = database_object.get_currency_from_provided_date(
+            currency_code='eur', date=date_fix
+        )
+        assert result == eur_currency
+
+    def test_is_date_in_database_with_no_date(
+        self, mocked_database_fix, date_fix
+    ):
+        database_object = Database()
+        database_object._get_date_filter.return_value = 'mocked'
+        mocked_collection = Mock()
+        mocked_collection.find_one.return_value = None
+        database_object.currency_collections = mocked_collection
+        result = database_object.is_date_in_database(date_fix)
+        assert result is False
+
+    def test_is_date_in_database_with_date(
+        self, mocked_database_fix, date_fix
+    ):
+        database_object = Database()
+        database_object._get_date_filter.return_value = 'mocked'
+        mocked_collection = Mock()
+        mocked_collection.find_one.return_value = date_fix
+        database_object.currency_collections = mocked_collection
+        result = database_object.is_date_in_database(date_fix)
+        assert result is True
+
+    def test_insert_currencies(
+        self, mocked_database_fix, usd_currency, eur_currency
+    ):
+        database_object = Database()
+        mocked_collection = Mock()
+        currencies = [eur_currency, usd_currency]
+        mocked_collection.insert_many.return_value = currencies
+        database_object.currency_collections = mocked_collection
+        result = database_object.insert_currencies(currencies)
+        assert result == currencies
